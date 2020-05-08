31a94b7fcab7fdce05540e8e7cd53595aa6ac9cc
Harsh Bharvada <harsh07bharvada@gmail.com>
2020-05-08 10:54:15 +0530

    Add redux persist and local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,25 @@
 import {combineReducers} from 'redux';
 import userReducer from './user/user.reducer';
-import cartReducer from './cart/cart.reducer'
+import cartReducer from './cart/cart.reducer';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
+// Telling redux that I am using localStorage and not sessionStorage for persisiting the particular reducers.
+// Persist Config is the config object which sets from where we need to start storing so key is root as rootReducer
+// then storage is for type of storage , it can be sessionStorage as well and whitelist is and array of independent
+//reducers which will be included in the storage.
+const persistConfig = {
+    key :'root',
+    storage,
+    whitelst :['cart']
+}
 
-export default combineReducers({
+// CombineReducers helps to combine all the small independent reducers to one root reducer
+const rootReducer =  combineReducers({
     user:userReducer,
     cart:cartReducer
 
-})
+});
+
+//Persist reducer passes the config and the reducer 
+export default persistReducer(persistConfig,rootReducer);
--- src/redux/store.reducer.js
+++ src/redux/store.reducer.js
@@ -1,9 +1,17 @@
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import {persistStore} from 'redux-persist';
 
+//Every Time action is called it will pass through these middlewares in the array
 const middlewares = [logger];
 
-const store = createStore(rootReducer,applyMiddleware(...middlewares));
+//This is the main store instance having root reducers and the middlewares.
+export const store = createStore(rootReducer,applyMiddleware(...middlewares));
 
-export default store;
+//Persistor from redux-persistor helps in persisting the part of data(from the specific reducers and & actions )
+// to stay in the LocalStorage / SessionStorage in the browser so that even after refresh the state of the cart (or
+// particular reducer is persisted)
+export const persistor = persistStore(store);
+
+export default { store,persistor};
